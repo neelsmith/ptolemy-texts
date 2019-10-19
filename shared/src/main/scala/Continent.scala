0ec2d323fa2995f9f8049aca6f3b05bd07961e3c
Neel Smith <dnsmith.neel@gmail.com>
2019-10-19 12:17:20 -0400

    Set up code build

---
+++ shared/src/main/scala/Continent.scala
@@ -0,0 +1,11 @@
+package edu.holycross.shot.ptolemy
+
+
+/**
+*/
+sealed trait Continent
+
+/**  */
+case object Europe extends Continent
+case object Asia extends Continent
+case object Africa extends Continent
---
+++ shared/src/test/scala/ContinentSpec.scala
@@ -0,0 +1,11 @@
+package edu.holycross.shot.ptolemy
+
+import org.scalatest.FlatSpec
+
+class ContinentSpec extends FlatSpec {
+
+  "The Continent trait" should "make sense" in pending
+
+
+
+}
