f71b34fc0315174857e4ed62c63bc59141dc7721
Neel Smith <dnsmith.neel@gmail.com>
2019-10-19 12:30:03 -0400

    Define continent trait

--- shared/src/main/scala/Continent.scala
+++ shared/src/main/scala/Continent.scala
@@ -3,9 +3,29 @@ package edu.holycross.shot.ptolemy
 
 /**
 */
-sealed trait Continent
+sealed trait Continent {
+  def name: String
+  /// def urn ...
+}
+
+object Continent  {
+  def apply(s: String) = {
+    s.toLowerCase match {
+      case "europe" => Europe
+      case "asia" => Asia
+      case "africa" => Africa
+      case _ => throw new Exception("Unrecognized name for continent: " + s)
+    }
+  }
+}
 
 /**  */
-case object Europe extends Continent
-case object Asia extends Continent
-case object Africa extends Continent
+case object Europe extends Continent {
+  def name = "Europe"
+}
+case object Asia extends Continent {
+  def name = "Asia"
+}
+case object Africa extends Continent {
+  def name = "Africa"
+}
